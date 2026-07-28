3f58c4d1570df446fe349f36ff58ebd588956e50
Jayalakshmi Sasidharan <jayalakshmi.nair1196@gmail.com>
2026-07-27 20:47:49 -0700

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.py
@@ -0,0 +1,63 @@
+from bisect import bisect_right
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        index = bisect_right(nums, target)
+        return index - 1 if index and nums[index - 1] == target else -1
+        # l = 0
+        # r = len(nums)
+
+        # while l < r:
+        #     mid = l + (r -l) // 2
+
+        #     if nums[mid] > target:
+        #         r = mid
+        #     else:
+        #         l = mid + 1
+
+        # return l - 1 if (l and nums[l - 1] == target) else -1
+
+    #     return self.binary_search(0, len(nums) - 1, nums, target)
+    
+    # def binary_search(self, left: int, right: int, nums: List[int], target):
+
+
+        # if left > right:
+        #     return -1
+        
+        # mid = (left + right) // 2
+
+        # if nums[mid] == target:
+        #     return mid
+        
+        # if nums[mid] > target:
+        #     return self.binary_search(left, mid - 1, nums, target)
+        # return self.binary_search(mid+1, right, nums, target)
+
+
+        
+        
+        # left = 0
+        # right = len(nums) - 1
+
+        # while left <= right:
+        #     mid = (left + right) // 2
+
+        #     if nums[mid] == target:
+        #         return mid
+            
+        #     if nums[mid] > target:
+        #         right = mid - 1
+        #     else:
+        #         left = mid + 1
+        # return -1
+
+
+        
+
+        # for i in range(len(nums)):
+        #     if nums[i] == target:
+        #         return i
+        # return -1
+
+        
